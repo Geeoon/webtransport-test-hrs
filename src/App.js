9b764d49e1458a13ed44c822e8b9ca21e6c16622
Geeoon Chung <geeoon13@gmail.com>
2022-10-23 14:03:23 -0700

    sequential datagrams

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import React, { useEffect, useMemo, useState } from 'react';
 
+const FPS = 20;
+let number = 0;
 /**
  * This function turns a buffer into an array of different buffers of specified size
  * Format:
@@ -18,13 +20,16 @@ function bufferPartitioner(buffer, max) { // theorhetical max 306000 bytes or 30
   let output = [];
   const buffer_size = max;
   let size = Math.ceil(buffer.length / buffer_size);
-  let id = Math.floor(Math.random() * 256);
+  number++;
+  if (number > 255) {
+    number = 0;
+  }
   for (let i = 0; i < size; i++) {
     let sub_buffer = buffer.subarray(i * (buffer_size - 3), (i+1) * (buffer_size - 3));
     let new_buffer = new Uint8Array(buffer_size);  // max is 1218
     new_buffer["0"] = size;
     new_buffer["1"] = i;
-    new_buffer["2"] = id;
+    new_buffer["2"] = number;
     new_buffer.set(sub_buffer, 3);
     output.push(new_buffer);
   }
@@ -40,14 +45,14 @@ function bufferPartitioner(buffer, max) { // theorhetical max 306000 bytes or 30
  * Parameters:
  *  array - the array of buffers to be concatonated
  */
-function bufferArrayConcat(array) {
+function bufferArrayConcat(array, size) {
   // make sure to replace 1200 with an actual variable from either the buffers a global const
-  let fake_buffer = new Array(array[0]["0"] * (1200 - 3));
+  let fake_buffer = new Array(array[0]["0"] * (size - 3));
   for (let element of array) {
     let index = element["1"];
     let data = element.subarray(3);
-    fake_buffer.splice(index * (1200 - 3), 0, ...data);
-    fake_buffer = fake_buffer.splice(0, (index + 1) * (1200 - 3));
+    fake_buffer.splice(index * (size - 3), 0, ...data);
+    fake_buffer = fake_buffer.splice(0, (index + 1) * (size - 3));
   }
   return new Uint8Array(fake_buffer);
 }
@@ -62,13 +67,17 @@ function App() {
   const [reader, set_reader] = useState(false);
   const [writer, set_writer] = useState(false);
   const [image_buffer, set_image_buffer] = useState(null);
-  const [old_image, set_old_image] = useState(null);
-  const transport = useMemo(async () => {
-    let wt = new WebTransport('https://usa.echo.webtransport.day');
-    set_reader(wt.datagrams.readable.getReader());
-    set_writer(wt.datagrams.writable.getWriter());
-    await wt.ready;
-    return wt;  // echo server
+  const [transport, set_transport] = useState(null);
+
+  useEffect(() => {
+    async function begin() {
+      let wt = new WebTransport('https://usa.echo.webtransport.day');
+      set_reader(wt.datagrams.readable.getReader());
+      set_writer(wt.datagrams.writable.getWriter());
+      await wt.ready;
+      set_transport(wt);  // echo server
+    }
+    begin();
   }, []);
 
   useEffect(() => {
@@ -83,14 +92,18 @@ function App() {
         await reader.ready;
         const {value, done} = await reader.read();
         set_data(value);
-        if (value["2"] !== id) {
+        if (value["2"] > id || id - value["2"] > 100) {  // if a new image is being received, display the old
           if (array.length > 0) {
-            set_old_image(bufferArrayConcat(array));
+            set_image_buffer(bufferArrayConcat(array, transport.datagrams.maxDatagramSize - 60));
           }
           array.length = 0;
           id = value["2"];
         }
         array.push(value);
+        if ((value["0"] - 1) === value["1"]) {  // if last message, you can display instantly
+          set_image_buffer(bufferArrayConcat(array, transport.datagrams.maxDatagramSize - 60));
+          array.length = 0;
+        }
         finished = done;
       }
     }
@@ -103,27 +116,30 @@ function App() {
     async function begin() {
       // sending to echo server
       // max write size 1218
-      while (true) {
-        let data_array = bufferPartitioner(sending_data, 1200);
-        for (let data of data_array) {
-          await writer.ready;
-          await writer.write(data);
-        }
+      let data_array = bufferPartitioner(sending_data, transport.datagrams.maxDatagramSize - 60);
+      for (let data of data_array) {
+        await writer.ready;
+        await writer.write(data);
       }
+      console.log("sent image");
     }
-    if (sending_data) {
+    /* if (sending_data) {
       begin();
+    } */
+    if (!sending_data) {
+      return;
     }
-  }, [sending_data, writer]);
+    const id = setInterval(begin, 1000 / FPS);
+    return () => clearTimeout(id);
+  }, [sending_data, writer, transport]);
 
   return (
     <div>
-      Image: <input type="file" id="img" accept="image/png, image/jpeg" onChange={ async (e) => {
+      Image: <input type="file" id="img" accept="image/png, image/jpeg, image/bmp, image/img" onChange={ async (e) => {
         if (!e.target.files[0]) return;
         set_sending_data(new Uint8Array(await e.target.files[0].arrayBuffer()));
-		  }} />
-      Received Data: { JSON.stringify(data) }<br />
-      <img alt="data received" src={ old_image && URL.createObjectURL(new Blob([old_image.buffer], { type: 'image/jpeg' }))}/>
+		  }} /><br />
+      <img alt="data received" src={ image_buffer && URL.createObjectURL(new Blob([image_buffer.buffer], { type: 'image/bmp' }))}/>
     </div>
   );
 }
